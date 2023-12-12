2e9af23f80584983e4236c979caa092c96afebfa
corner <rileylnorton05@gmail.com>
2023-12-12 16:28:47 +0000

    Day 12

--- day10/solve.py
+++ day10/solve.py
@@ -143,18 +143,6 @@ def part2(data):
     return total
 
 
-data = """FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L"""
-
-
 def main():
     day = int(__file__.split("\\")[-2][-2:])
     data = get_data(day=day, year=YEAR)
