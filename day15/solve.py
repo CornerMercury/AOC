8f9726a35a64867e1563f36c01247afd88b50975
corner <rileylnorton05@gmail.com>
2023-12-18 18:51:47 +0000

    Day 17

--- day15/solve.py
+++ day15/solve.py
@@ -53,9 +53,6 @@ def part2(data):
     return res
 
 
-data = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
-
-
 def main():
     day = int(__file__.split("\\")[-2][-2:])
     data = get_data(day=day, year=YEAR)
