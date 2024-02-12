d7a17f7b308b94de679b14fb5b9ae81760cee42a
corner <rileylnorton05@gmail.com>
2024-02-12 00:18:35 +0000

    Removed txt files

--- generate_days.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-for day in range(1, 26):
-    directory_name = f"day{day:02}"
-    if not os.path.exists(directory_name):
-        os.makedirs(directory_name)
-        shutil.copy("solve_template.py", f"{directory_name}/solve.py")
-        open(f"{directory_name}/__init__.py", "a").close()
