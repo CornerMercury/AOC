db019ab9b6037ea0f338ddd24ffe4c4979084482
riley <rileylnorton05@gmail.com>
2024-12-02 05:18:16 +0000

    2024 python day2

--- 2024/python/day02/solve.py
+++ 2024/python/day02/solve.py
@@ -4,12 +4,32 @@ YEAR = 2024
 
 def part1(data):
     l = data.split("\n")
-    return None
+    t=0
+    for s in l:
+        i = *map(int, s.split()),
+        t+=(max(abs(b-a) for a,b in zip(i, i[1:]))<=3)and(min(abs(b-a) for a,b in zip(i, i[1:]))>=1) and (([abs(b-a) for a,b in zip(i, i[1:])] == [b-a for a,b in zip(i, i[1:])])  or([abs(b-a) for a,b in zip(i, i[1:])] == [a-b for a,b in zip(i, i[1:])]))
+    return t
 
 
 def part2(data):
     l = data.split("\n")
-    return None
+    t=0
+    for s in l:
+        z = *map(int, s.split()),
+        i=z[::]
+        l=(max(abs(b-a) for a,b in zip(i, i[1:]))<=3)and(min(abs(b-a) for a,b in zip(i, i[1:]))>=1) and (([abs(b-a) for a,b in zip(i, i[1:])] == [b-a for a,b in zip(i, i[1:])])  or([abs(b-a) for a,b in zip(i, i[1:])] == [a-b for a,b in zip(i, i[1:])]))
+        if l:
+            t+=1
+        else:
+            for c in range(len(z)):
+                i=z[:c] +z[c+1:]
+                l=(max(abs(b-a) for a,b in zip(i, i[1:]))<=3)and(min(abs(b-a) for a,b in zip(i, i[1:]))>=1) and (([abs(b-a) for a,b in zip(i, i[1:])] == [b-a for a,b in zip(i, i[1:])])  or([abs(b-a) for a,b in zip(i, i[1:])] == [a-b for a,b in zip(i, i[1:])]))
+                if l:
+                    t+=1
+                    b=True
+                    break
+                
+    return t
 
 
 def main():
