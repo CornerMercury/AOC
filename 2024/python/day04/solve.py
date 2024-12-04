7149a9141ec4986f6b258585121b820a4644e065
riley <rileylnorton05@gmail.com>
2024-12-04 05:33:24 +0000

    2024 python day 4

--- 2024/python/day04/solve.py
+++ 2024/python/day04/solve.py
@@ -4,12 +4,60 @@ YEAR = 2024
 
 def part1(data):
     l = data.split("\n")
-    return None
-
+    t=0
+    for y in range(len(l)):
+        for x in range(len(l[0])):
+            if l[y][x] == 'X':
+                try:
+                    if ''.join(l[y][x+c] for c in range(0, 4)) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y][x-c] for c in range(0, 4)if x-c>=0) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y+c][x] for c in range(0, 4)) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y-c][x] for c in range(0, 4)if y-c>=0) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y-c][x-c] for c in range(0, 4)if (y-c>=0) and (x-c>=0)) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y-c][x+c] for c in range(0, 4)if y-c>=0) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y+c][x+c] for c in range(0, 4)) == 'XMAS':t+=1
+                except:pass
+                try:
+                    if ''.join(l[y+c][x-c] for c in range(0, 4)if x-c>=0) == 'XMAS':t+=1
+                except:pass
+                
+    return t
 
 def part2(data):
+#     data="""MMMSXXMASM
+# MSAMXMSMSA
+# AMXSXMAAMM
+# MSAMASMSMX
+# XMASAMXAMM
+# XXAMMXXAMA
+# SMSMSASXSS
+# SAXAMASAAA
+# MAMMMXMMMM
+# MXMXAXMASX"""
     l = data.split("\n")
-    return None
+    t=0
+    for y in range(1,len(l)-1):
+        for x in range(1,len(l[0])-1):
+            if l[y][x] == 'A':
+                a=l[y-1][x-1]
+                b,c=l[y+1][x-1],l[y-1][x+1]
+                d=l[y+1][x+1]
+                if ((a==b and c==d) or (a==c and b==d)) and sorted(a+d)==['M','S']:
+                    t+=1
+    print(t)
+    return t
 
 
 def main():
