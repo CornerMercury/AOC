0e7d015e6621ab8f4d393d554af5cb2d07d16bb4
riley <rileylnorton05@gmail.com>
2024-12-05 05:27:54 +0000

    2024 python day 5

--- 2024/python/day05/solve.py
+++ 2024/python/day05/solve.py
@@ -1,15 +1,61 @@
 from aocd import get_data, submit
+from time import sleep
 
 YEAR = 2024
 
 def part1(data):
     l = data.split("\n")
-    return None
+    o = {}
+    i=0
+    while l[i]!='':
+        a,b = l[i].split("|")
+        if a not in o:
+            o[a]=set()
+        o[a].add(b)
+        i+=1
+    t=0
+    for s in l[i+1:]:
+        ns = s.split(',')
+        c=True
+        for i in range(len(ns)):
+            for j in range(0, i):
+                if (ns[i] in o) and (ns[j] in o[ns[i]]):
+                    c = False
+                    break
+            if not c:
+                break
+        else:
+            t+=int(ns[len(ns)//2])
+    return t
 
 
 def part2(data):
     l = data.split("\n")
-    return None
+    o = {}
+    i=0
+    while l[i]!='':
+        a,b = l[i].split("|")
+        if a not in o:
+            o[a]=set()
+        o[a].add(b)
+        i+=1
+    t=0
+    for s in l[i+1:]:
+        ns = s.split(',')
+        c=True
+        i=0
+        while i < len(ns):
+            j=0
+            while j < i:
+                if (ns[i] in o) and (ns[j] in o[ns[i]]):
+                    c = False
+                    ns.insert(i, ns.pop(j))
+                    i-=1
+                j+=1
+            i+=1
+        if not c:
+            t+=int(ns[len(ns)//2])
+    return t
 
 
 def main():
