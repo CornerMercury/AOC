cbcdf0b45f1679bc16046b5fc21e74f780e70474
riley <rileylnorton05@gmail.com>
2024-12-11 05:25:12 +0000

    2024 python day 11

--- 2024/python/day11/fast.py
+++ 2024/python/day11/fast.py
@@ -5,21 +5,19 @@ YEAR=2024
 
 def part1(data):
     l = data.strip().split(" ")
-    for _ in range(25):
-        i = 0
-        while i < len(l):
-            if l[i] == '0':
-                l[i] = '1'
-            elif len(l[i]) % 2 == 0:
-                t = l[i]
-                l[i] = str(int(t[:len(t)//2]))
-                i+=1
-                l.insert(i, str(int(t[len(t)//2:])))
-            else:
-                l[i] = str(int(l[i]) * 2024)
-            i += 1
-            
-    return len(l)
+    @cache
+    def split(stone, n):
+        if n == 0:
+            return 1
+        if stone == 0:
+            return split(1, n - 1)
+        elif len(str(stone)) % 2 == 0:
+            s = str(stone)
+            return split(int(s[:len(s)//2]), n-1) + split(int(s[len(s)//2:]), n-1)
+        else:
+            return split(stone * 2024, n - 1)
+    
+    return sum(split(int(c), 25) for c in l)
 
 
 def part2(data):
