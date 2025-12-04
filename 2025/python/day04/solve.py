7ca01a3206a90ef469766c5c3297aaf336c07204
CornerMercury <corner@mercury.com>
2025-12-04 15:44:59 +0000

    feat: solve day 4

--- 2025/python/day04/solve.py
+++ 2025/python/day04/solve.py
@@ -5,12 +5,50 @@ YEAR = 2025
 
 def part1(data):
     l = data.split("\n")
-    return None
+    t=0
+    for i,r in enumerate(l):
+        for j,c in enumerate(r):
+            rolls = 0
+            if c != "@":
+                continue
+            for dr in range(-1,2):
+                nr = i + dr
+                if not (0 <= nr < len(l)):
+                    continue 
+                for dc in range(-1,2):
+                    nc = j + dc
+                    if not (0 <= nc < len(r)) or (dc == dr == 0):
+                        continue
+                    rolls += l[nr][nc] == "@"
+            if rolls < 4:
+                t+=1
+    return t
 
 
 def part2(data):
     l = data.split("\n")
-    return None
+    new_t = 0
+    old_t = -1
+    while old_t != new_t:
+        old_t = new_t
+        for i,r in enumerate(l):
+            for j,c in enumerate(r):
+                rolls = 0
+                if c != "@":
+                    continue
+                for dr in range(-1,2):
+                    nr = i + dr
+                    if not (0 <= nr < len(l)):
+                        continue 
+                    for dc in range(-1,2):
+                        nc = j + dc
+                        if not (0 <= nc < len(r)) or (dc == dr == 0):
+                            continue
+                        rolls += l[nr][nc] == "@"
+                if rolls < 4:
+                    new_t += 1
+                    l[i] = l[i][:j] + "." + l[i][j+1:]
+    return new_t
 
 
 def main():
