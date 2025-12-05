361f59ce035848daeb5a18fe2e0a1d2688c6de09
CornerMercury <corner@mercury.com>
2025-12-05 13:19:52 +0000

    feat: solve day 5

--- 2025/python/day05/solve.py
+++ 2025/python/day05/solve.py
@@ -5,12 +5,57 @@ YEAR = 2025
 
 def part1(data):
     l = data.split("\n")
-    return None
+    ranges = []
+    for i in range(len(l)):
+        if l[i] == "":
+            break
+        L, R = l[i].split("-")
+        ranges.append((int(L), int(R)))
+
+    t = 0
+    for i in range(i + 1, len(l)):
+        n = int(l[i])
+        for j in range(len(ranges)):
+            if ranges[j][0] <= n <= ranges[j][1]:
+                t += 1
+                break
+    return t
 
 
 def part2(data):
     l = data.split("\n")
-    return None
+    ranges = set()
+    for i in range(len(l)):
+        if l[i] == "":
+            break
+        L, R = l[i].split("-")
+        ranges.add((int(L), int(R)))
+
+    t = 0
+    while ranges:
+        new_ranges = set()
+        r = next(iter(ranges))
+        left, right = r
+        t += right - left + 1
+        for r in ranges:
+            new_left, new_right = r
+            if left <= new_left <= right and right <= new_right:
+                if right + 1 <= new_right:
+                    new_ranges.add((right + 1, new_right))
+            elif new_left <= left and left <= new_right <= right:
+                if new_left <= left - 1:
+                    new_ranges.add((new_left, left - 1))
+            elif new_left < left and right < new_right:
+                new_ranges.add((new_left, left - 1))
+                new_ranges.add((right + 1, new_right))
+            elif left <= new_left <= right and left <= new_right <= right:
+                pass
+            else:
+                new_ranges.add(r)
+
+        ranges = new_ranges
+
+    return t
 
 
 def main():
